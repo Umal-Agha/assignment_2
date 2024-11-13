8600f5af13b15727661abf414c308e2ac4ad4842
Saira-Agha <sairaagha1999@gmail.com>
2024-11-13 17:26:54 +0500

    first commit

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,2 +1,25 @@
+export default function Navbar() {
+  return (
+    <div className="h-[91px] w-[1322px] absolute left-[59px] text-white font-sans bg-blue-500">
+      <div className="h-[91px] w-[1322px]">
+        <div className="w-[815px] h-[58px] absolute top-[16px] left-[364px] bg-blue-300">
+          <div className="w-[189px] h-[52px] absolute top-3 left-[626px] gap-[45px]">
+            <div className="w-[41px] h-[22px]">
+              <div className="w-[41px] h-[22px]">
+                <button className="font-bold text-sm leading-[22px] tracking-[0.2px] text-right">Login</button>
+              </div>
+            </div>
+            <div>
+              
+            </div>
+          </div>
+          <div>
 
-  
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,8 @@
-
+import Navbar from "@/app/components/navbar"
+export default function Home() {
+  return (
+    <div className="h-[1132px] w-[1440px] bg-[#252B42]">
+      <Navbar />
+    </div>
+  )
+}
